28bb53c4e71e640032469c0769ee11c98a344f5f
emachizy <emachi2011@gmail.com>
2025-04-05 15:50:54 -0700

    updated .env file

--- src/firebase.js
+++ src/firebase.js
@@ -9,12 +9,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyChr0aNm5_6vE0CLpzpT9K42iZKLiYwYrw",
-  authDomain: "compass-trybe.firebaseapp.com",
-  projectId: "compass-trybe",
-  storageBucket: "compass-trybe.firebasestorage.app",
-  messagingSenderId: "206366659207",
-  appId: "1:206366659207:web:62ada61d56781caa191e22",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
